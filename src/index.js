11c847dff78a4da7c6ffed86ecb5e9833f3398a5
ang-rea-dep <erface31@gmail.com>
2020-04-20 20:20:36 +0300

    testing app

--- src/index.js
+++ src/index.js
@@ -4,11 +4,25 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
+const myelements = (
+    <table>
+        <th>
+            <td>1</td>
+            <td>2</td>
+        </th>
+        <tbody>
+            <tr>
+                <td>123</td>
+                <td>asdf</td>
+            </tr>
+        </tbody>
+    </table>
+)
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+    <React.StrictMode>
+        <App />
+    </React.StrictMode>,
+    document.getElementById('root')
 );
 
 // If you want your app to work offline and load faster, you can change
