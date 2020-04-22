9e886b879c55bc5c636865d24e3cbeab66d7f1a9
ang-rea-dep <erface31@gmail.com>
2020-04-23 01:31:07 +0300

    react hook example

--- src/component/Car.js
+++ src/component/Car.js
@@ -1,24 +1,57 @@
-import React, {Component} from "react";
+import React, { useState, useEffect } from "react";
+import randomcolor from "randomcolor"
 
-class Car extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            mystate: ''
+function Car(){
+    const [count, setCount] = useState(0)
+    const [answer, setAnswer] = useState("Zero")
+    const [color, setColor] = useState("")
+    const [stop, setStop] = useState(false)
+    const [timer, setTimer] = useState(null);
+
+    function increment(){
+        setCount(precount => precount + 1)
+        if(count < 0){
+            setAnswer("No")
+        }
+        else{
+            setAnswer("Yes")
         }
     }
-    onChageState = (event) => {
-        this.setState({mystate: event.target.value})
+    function decrement(){
+        setCount(precount => precount -1)
+        if(count < 0){
+            setAnswer("No")
+        }
+        else{
+            setAnswer("Yes")
+        }
     }
-    render() {
-        return (
-            <div>
-                <form action="">
-                    <input type="text" onChange={this.onChageState}/>
-                    <p>{this.state.mystate}</p>
-                </form>
-            </div>
-        );
-    }
+
+    useEffect(() => {
+        if(stop){
+            clearInterval(timer)
+        }
+        else{
+            const intervalId = setInterval(() => {
+                setCount(precount => precount + 1)
+            }, 1000)
+            setTimer(intervalId);
+        }
+        return () => clearInterval(timer)
+        },[stop])
+
+    useEffect(() => {
+        setColor(randomcolor())
+    }, [answer, count])
+
+    return(
+        <>
+            <h1 style={{color: color}}>{count}</h1>
+            <p style={{color: color}}>{answer}</p>
+            <button type="button" onClick={increment}>increment</button>
+            <button type="button" onClick={decrement}>decrement</button>
+            <button type="button" onClick={() => setStop(true)}>stop</button>
+        </>
+    )
 }
 export default Car;
